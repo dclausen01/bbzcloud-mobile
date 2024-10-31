56e17c68c42dcff235c8940760a477dfba5d83b9
Dennis Clausen <Dennis.Clausen@bbz-rd-eck.de>
2024-10-31 18:10:13 +0100

    fix

--- utils/urlHandler.ts
+++ utils/urlHandler.ts
@@ -1,7 +1,7 @@
 import { router, Href } from 'expo-router';
 
 type TabInfo = {
-  tab: 'moodle' | 'office' | 'schulcloud' | 'wiki' | 'untis';
+  tab: 'moodle' | 'wiki';
   route: Href<string>;
 };
 
@@ -12,14 +12,8 @@ export const getTabFromUrl = (url: string): TabInfo | null => {
     switch (parsedUrl.hostname) {
       case 'portal.bbz-rd-eck.com':
         return { tab: 'moodle', route: '/(tabs)/moodle' as Href<string> };
-      case 'cryptpad.fr':
-        return { tab: 'office', route: '/(tabs)/office' as Href<string> };
-      case 'app.schul.cloud':
-        return { tab: 'schulcloud', route: '/(tabs)/schulcloud' as Href<string> };
       case 'wiki.bbz-rd-eck.com':
         return { tab: 'wiki', route: '/(tabs)/wiki' as Href<string> };
-      case 'neilo.webuntis.com':
-        return { tab: 'untis', route: '/(tabs)/untis' as Href<string> };
       default:
         return null;
     }
