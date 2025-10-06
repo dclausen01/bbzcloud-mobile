f580e65b06e8dd2ae439f1d76031c47d581d2b75
dclausen01 <32433965+dclausen01@users.noreply.github.com>
2025-10-06 08:38:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bbzcloud-temp',
+  webDir: 'dist'
+};
+
+export default config;
